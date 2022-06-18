d01ae28fd45816fd68c4cd4d0e7c645f58bc3b84
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-06-18 21:33:36 +0100

    create sauce path added authorisation

--- routes/sauces.js
+++ routes/sauces.js
@@ -4,7 +4,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const saucesCtrl = require('../controllers/sauces');
 
-  router.post('/', multer, saucesCtrl.createSauce);  
+  router.post('/',auth, multer, saucesCtrl.createSauce);  
   router.get('/', saucesCtrl.getAllSauces);
   router.put('/:id', auth, multer, saucesCtrl.modifySauce);
   router.get('/:id', saucesCtrl.getOneSauce);
